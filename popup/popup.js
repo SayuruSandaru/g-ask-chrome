ecc9a26d3c009baa26f8d515d52d812310a84ebc
Sayuru Sandaru <sayurusadaru12@gmail.com>
2023-07-26 12:12:17 +0530

    UI update

--- popup/popup.js
+++ popup/popup.js
@@ -1,10 +1,12 @@
 document.addEventListener('DOMContentLoaded', function () {
     const apiKeyInput = document.getElementById('api-key');
     const saveButton = document.getElementById('save-button');
+    const clearButton = document.getElementById('clear-button');
+    const toggleButton = document.getElementById('toggle-button');
 
     // Load the stored API key from local storage, if available
     const storedApiKey = localStorage.getItem('gask-api-key');
-    if (storedApiKey) {
+    if (storedApiKey != "") {
       const maskedKey = storedApiKey.slice(0, 5) + '*****' + storedApiKey.slice(-5);
       document.getElementById('stored-key').textContent = `Stored API Key: ${maskedKey}`;
     }
@@ -18,5 +20,20 @@ document.addEventListener('DOMContentLoaded', function () {
         document.getElementById('stored-key').textContent = `Stored API Key: ${maskedKey}`;
         apiKeyInput.value = ''; // Clear the input field after saving
       }
+    });
+
+    toggleButton.addEventListener('click', function () {
+      const icon = document.getElementById('icon-btn');
+      if(apiKeyInput.type === "password"){
+        apiKeyInput.type = "text";
+        icon.src = "../assets/eye.svg";
+      }else{
+        apiKeyInput.type = "password";
+        icon.src = "../assets/eye-off.svg";
+      }
+    });
+
+    clearButton.addEventListener('click', function () {
+      document.getElementById("api-key").value = "";
     });
   });
